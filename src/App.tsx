5ccadba0da0a39de46b8fded967a872f1817de1d
dommentee <chuku0929@gmail.com>
2021-12-02 15:06:17 -0500

    connect front to back-end

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,37 @@ import axios from 'axios';
 
 import './sass/App.scss';
 
+//import types
+import { Procedure} from './helpers/types';
+
 
 const App = () => {
+  //set state of data
+  const [procedures, setProcedures] = useState<Array<Procedure>>([])//need to be array of porocedures
   const getProcedures = () => {
     axios.get(`http://localhost:3001/procedures`)
-    .then((response) => {
-      response.data
-    })
+    .then((response) => setProcedures(response.data.rows),
+    (err) => console.error(err.message));
   }
-
+  console.log(procedures);
+  
+  useEffect(() => {
+    getProcedures()
+  },[])
+  
   return (
-    <h3>Home</h3>
+    <div>
+    {
+      procedures ? (
+        procedures.map((procedure) => (
+        <div>
+          {procedure.name}
+        </div>
+        ))
+      ): <></>
+    }
+      <h3>Home</h3>
+    </div>
   )
 
 }
