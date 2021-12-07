f0843ef89801f6ee2f0bb9b001335f7e7533f8c8
dommentee <chuku0929@gmail.com>
2021-12-07 00:05:03 -0500

    can search from postman

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,15 @@ import Signup from './pages/Signup';
 import Footer from './components/Footer';
 import ProcedureForm from './components/ProcedureForm';
 import Profile from './pages/Profile';
+import { User } from './helpers/user';
 
-
+const procedureApi = 'https://still-plateau-52039.herokuapp.com/procedures/';
 const App = () => {
-  //navaget is used to route for different button 
+  ///USERS
   //state of user 
   let [user, setUser] = useState(true)
+
+  ////API 
   //set state of data
   const [procedures, setProcedures] = useState<Array<Procedure>>([])//need to be array of porocedures
   const getProcedures = () => {
@@ -27,7 +30,7 @@ const App = () => {
     .then((response) => setProcedures(response.data.rows),
     (err) => console.error(err.message));
   }
-  console.log(procedures);
+  // console.log(procedures);
   
   //create fuction
   const handleCreate = (newProcedure: Procedure) => {
@@ -35,40 +38,66 @@ const App = () => {
     .then((response) => getProcedures(),
       (err) => console.error(err.message));
       console.log(newProcedure);
-      
   }
-  
   //update
   const handleUpdate = (editProcedure: Procedure) => {
     axios.put('https://still-plateau-52039.herokuapp.com/procedures/' + editProcedure.procedure_id, editProcedure)
     .then((response) => getProcedures(),
     (err) => console.error(err.message));
-    console.log(editProcedure);
-        
+    console.log(editProcedure);     
   }
- 
   //delete
   const handleDelete = (e: any) => {
     axios.delete('https://still-plateau-52039.herokuapp.com/procedures/' + e.target.value)
     .then((response) => getProcedures(),
     (err) => console.error(err.message));
   }
+
   
+  //users
+  const getUser = () => {
+    console.log(user);
+  }
+
+
+  //create
+  const createUser = (newUser: User) => {
+    axios.post('http://localhost:3001/users', newUser)
+    .then((response) => getUser(),
+    (err) => console.error(err.message));
+  }
+  
+  ///SEARCH 
+  let [searchInput, setSearchInput] = useState(null as any)
+  const makeRequest = (filter: any) => {
+    axios.get(procedureApi + 'search/' + filter)
+    .then((response) => getProcedures())
+    
+  }
+
   useEffect(() => {
-    getProcedures()
+    makeRequest('')
+    // getProcedures()
   },[])
-  
+
+  const handleSearch = (e: any) => {
+    e.preventDefault();
+    makeRequest(`${searchInput}`)
+    console.log(procedures); 
+  }
+
   return (
     <div className="container">
       <Router>
         <div className="header">
           <div className="search-form-warp">
             <div className="home-button"><Link to="/">Home</Link></div>
-            <form className="seach-form">
+            <form onSubmit={handleSearch} className="seach-form">
               <input 
-                type="input" 
+                type="input" onChange={(e: any) => setSearchInput(e.target.value)} 
                 className="searh-input"
                 placeholder="search procedure"
+                value={searchInput}
                 />
               <input type="submit" value="search" className="submit-search" />
             </form>
@@ -86,11 +115,19 @@ const App = () => {
             }
           </div>
         </div>
+        {
+            searchInput ? (
+              <div>
+                
+                
+              </div>
+            ): <></>
+          }
         <Routes>
           <Route path="/" element={
             <Home />
           }/>
-          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signup" element={<Signup createUser={createUser}/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/contribute" element={<ProcedureForm  handleCreate={handleCreate}/>}/>
           <Route path="/profile" element={<Profile
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Signup = () => {
+
+
+const Signup = (props: any) => {
+
+    const defaultForm = {user_name: '', password: ''}
+    let [newUser, setNewUSer] = useState(defaultForm)
+    const handleChange = (e:any) => {
+        setNewUSer({...newUser, [e.target.name]: e.target.value})  
+    } 
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        props.createUser(newUser)        
+    }
+
     return (
         <div>
             <h3>Sign up</h3>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="user name">user name</label>
+                <input
+                    type="text"
+                    name="user_name"
+                    onChange={handleChange}
+                    value={newUser.user_name}
+                    required
+                    className="input"
+                />
+                <label htmlFor="password">password</label>
+                <input 
+                    type="text"
+                    name="password"
+                    onChange={handleChange}
+                    value={newUser.password}
+                    className="input"
+                />
+             <input className="send" type="submit" value="SUBMIT"></input>
+            </form>
         </div>
     )
 }
