fa4fb1ea53e0e7271d378d4c21bc0cd217bd553b
dommentee <chuku0929@gmail.com>
2022-02-25 15:20:37 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,6 @@ const App = () => {
   xhr.open('POST', 'https://enigmatic-retreat-81576.herokuapp.com/login', true);
   xhr.open('GET', 'https://enigmatic-retreat-81576.herokuapp.com/login', true);
   xhr.open('GET', 'https://enigmatic-retreat-81576.herokuapp.com/users', true);
-  xhr.open('POST', 'https://enigmatic-retreat-81576.herokuapp.com/users', true);
   xhr.withCredentials = true;
   xhr.send(null);
 
